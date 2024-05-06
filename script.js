7989aa31ead51219d7515acca5209e52e2d73064
jsmalling1 <132801710+jsmalling1@users.noreply.github.com>
2024-05-06 06:16:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Get the button
+var mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach(function(navLink) {
+    navLink.addEventListener("click", function(event) {
+      event.preventDefault();
+
+      var targetId = this.getAttribute("href").substring(1); // Get target section ID
+      var targetSection = document.getElementById(targetId); // Get target section element
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" }); // Smooth scroll to target section
+      }
+    });
+  });
+});
